83f7838328dfa41cf85760460f7aef33d828f799
Ilya Lasy <ilyalas@gmail.com>
2022-03-18 11:50:03 +0200

    added concepts teachers for bst/convai/ed/wow

--- parlai/tasks/convai2/agents.py
+++ parlai/tasks/convai2/agents.py
@@ -217,3 +217,17 @@ class SampleTeacher(YamlTeacher):
             os.path.dirname(__file__), f'test/convai2_{fold}.yml'
         )
         super().__init__(opt, shared)
+
+class ConceptsTeacher(FbDeprecatedDialogTeacher):
+    def __init__(self, opt, shared=None):
+        opt = copy.deepcopy(opt)
+        try:
+            cands = opt['task'].split(":")[2]
+            use_cands = False if cands == 'no_cands' else True
+        except Exception:
+            use_cands = True
+        opt['datafile'] = _path(opt, 'self_original_concepts', use_cands)
+        super().__init__(opt, shared)
+
+class NormalizedConceptsTeacher(NormalizedTeacherTrait, ConceptsTeacher):
+    pass
